fd57952130b94f4410979408d39dc028e6fc71b0
Saviour <1907saviour@gmail.com>
2020-05-30 15:28:23 +0530

    added full screen dialog to get data from user

--- app/src/main/java/com/saviour/todoapp/DialogNewTask.java
+++ app/src/main/java/com/saviour/todoapp/DialogNewTask.java
@@ -1,26 +1,41 @@
 package com.saviour.todoapp;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class DialogNewTask extends DialogFragment {
 
     public static final String TAG = "Create new task";
     private MaterialToolbar toolbar;
     private MaterialButton btnSave;
+    final Calendar myCalendar = Calendar.getInstance();
+    private String tempDate, tempTime;
+    private List<EditText> editTextList;
+    private TextInputEditText titleEditText, dueByEditText, notifyEditTxt;
 
     public static DialogNewTask display(FragmentManager fragmentManager){
         DialogNewTask dialogNewTask = new DialogNewTask();
@@ -32,6 +47,8 @@ public class DialogNewTask extends DialogFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setStyle(DialogFragment.STYLE_NORMAL,R.style.AppTheme_Dialog);
+
+        editTextList = new ArrayList<EditText>();
     }
 
     @Nullable
@@ -42,15 +59,52 @@ public class DialogNewTask extends DialogFragment {
 
         toolbar = view.findViewById(R.id.toolbar);
         btnSave = view.findViewById(R.id.save);
+        titleEditText = view.findViewById(R.id.title);
+        dueByEditText = view.findViewById(R.id.due);
+        notifyEditTxt = view.findViewById(R.id.notify);
+
+        editTextList.add(titleEditText);
+        editTextList.add(dueByEditText);
+        editTextList.add(notifyEditTxt);
+
+        dueByEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateTimePicker(dueByEditText);
+            }
+        });
+
+        notifyEditTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateTimePicker(notifyEditTxt);
+            }
+        });
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dismiss();
+                if (validateTextFields(editTextList)) {
+                    Toast.makeText(getActivity(), "Saved", Toast.LENGTH_SHORT).show();
+                    dismiss();
+                }
             }
         });
          return view;
 
+    }
+
+    private boolean validateTextFields(List<EditText> editTextList) {
+        boolean isEmpty = false;
+        for (EditText editText : editTextList) {
+            if (TextUtils.isEmpty(editText.getText())) {
+                editText.setError("Please fill out this field");
+                isEmpty = true;
+            }
+        }
+        if (isEmpty) return false;
+
+        return true;
     }
 
     @Override
@@ -68,6 +122,7 @@ public class DialogNewTask extends DialogFragment {
     @Override
     public void onStart() {
         super.onStart();
+
         Dialog dialog = getDialog();
         if(dialog != null){
             int width = ViewGroup.LayoutParams.MATCH_PARENT;
@@ -75,5 +130,37 @@ public class DialogNewTask extends DialogFragment {
             dialog.getWindow().setLayout(width,height);
             dialog.getWindow().setWindowAnimations(R.style.AppTheme_Slide);
         }
+    }
+
+    private void showDateTimePicker(final EditText editText) {
+        new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String DateFormat = "dd/MM/YYYY";
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DateFormat);
+
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                tempDate = simpleDateFormat.format(myCalendar.getTime());
+
+                new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        String TimeFormat = "hh:mm a";
+                        SimpleDateFormat simpleTimeFormat = new SimpleDateFormat(TimeFormat);
+
+                        myCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        myCalendar.set(Calendar.MINUTE, minute);
+
+                        tempTime = simpleTimeFormat.format(myCalendar.getTime());
+
+
+                        editText.setText(tempDate + ", " + tempTime);
+                    }
+                }, myCalendar.get(Calendar.HOUR_OF_DAY), myCalendar.get(Calendar.MINUTE), true).show();
+            }
+        }, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 }
